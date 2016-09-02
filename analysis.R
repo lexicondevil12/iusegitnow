649b0b982be455b091fc1931a82cf342809fd38f
lexicondevil12 <dcarver12@gmail.com>
2016-09-02 15:06:50 -0500

    an ever growing pile of parrot droppings

---
+++ analysis.R
@@ -0,0 +1,10 @@
+df_test <- data.frame(x = rnorm(10), y = rbinom(10, 1, .5))
+
+adder <- function(df) {
+  out <- df[,1] + df[,2]
+  out
+}
+
+y <- adder(df_test)
+
+#p.s. your breath smells like you wiped your ass with your teeth
